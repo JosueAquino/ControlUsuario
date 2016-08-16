6a8e35aa75b44a2c78f772dfd3eab59c214da6a9
sindyRaquely <sindyRaquely@Sindy>
2016-08-16 19:24:46 -0300

    sind_Conex

--- src/util/Conexion.java
+++ src/util/Conexion.java
@@ -18,7 +18,7 @@ import javax.swing.JOptionPane;
  * @author alum.fial8
  */
 public class Conexion {
-    private static final String URL = "jdbc:mysql://localhost:3306/";
+    private static final String URL = "jdbc:mysql://localhost:3306/Proy1Github";
     private static final String DRIVER ="com.mysql.jdbc.Driver";
     private static final String USER = "root";
     private static final String PASS = "root";

9bbdb6edfec11229d097949e26fb6b1daa7d97a6
JOSS <JOSS@Josue>
2016-08-16 19:55:27 -0500

    josue UsuarioDAO interface Operaciones

--- src/dao/usuarioDAO.java
+++ src/dao/usuarioDAO.java
@@ -5,10 +5,44 @@
  */
 package dao;
 
+import dto.usuarioDTO;
+import interfaces.Operaciones;
+import java.util.List;
+
 /**
  *
  * @author JOSS
  */
-public class usuarioDAO {
+public class usuarioDAO implements Operaciones<usuarioDTO>{
+
+    @Override
+    public int create(usuarioDTO e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int update(usuarioDTO e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int delete(Object key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<usuarioDTO> readAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public usuarioDTO read(Object key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean search(String e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- src/interfaces/Operaciones.java
+++ src/interfaces/Operaciones.java
@@ -5,10 +5,17 @@
  */
 package interfaces;
 
+import java.util.List;
+
 /**
  *
  * @author JOSS
  */
-public interface Operaciones {
-    
+public interface Operaciones <Entidad>{
+    public int create(Entidad e);
+    public int update(Entidad e);
+    public int delete(Object key);
+    public List<Entidad> readAll();
+    public Entidad read(Object key);
+    public boolean search(String e);
 }
